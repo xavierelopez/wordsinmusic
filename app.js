795011bc25d667e56ab8b309c6d65699feb17488
Xavier E. López <xavierelopez@gmail.com>
2011-10-27 21:29:36 -0400

    Fixing port issue

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require.paths.unshift(__dirname + '/vendor/node_modules');
 require('./lib/wordsinmusic');
 require('./server.js').start({
-	portNumber: 3000, 
+	portNumber: process.env.PORT || 3000, 
 	staticDirectory: __dirname + '/public'
 });
 
